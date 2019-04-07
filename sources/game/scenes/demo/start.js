408a467fa38286ec02431ff15a9e5d2a045c2daf
Alexandre Blondeau <deformhead@gmail.com>
2019-04-07 12:23:28 +0200

    update 'demo'

--- sources/game/scenes/demo/start.js
+++ sources/game/scenes/demo/start.js
@@ -19,10 +19,10 @@ function start() {
     this.world.add(new Entity('hero', [
 
         new Direction('DOWN'),
-        new Image(this.assets.images.demo['wizard'], [32, 32], [3, 2]),
+        new Image(this.assets.images.demo['wizard'](), [32, 32], [3, 2]),
         new Input(['KEY_UP', 'KEY_RIGHT', 'KEY_DOWN', 'KEY_LEFT']),
         new Position(this.size.width / 2 - 16, this.size.height / 2 - 16),
-        new Spritesheet(this.assets.datasets.demo['wizard'])
+        new Spritesheet(this.assets.datasets.demo['wizard']())
     ]));
 
     this.world.add(new Entity('framerate', [
@@ -30,8 +30,8 @@ function start() {
         new Position(8, 8),
         new Text('fps:60'),
         new Framerate(),
-        new Alphabet(this.assets.images.demo['font'], [16, 32]),
-        new Spritesheet(this.assets.datasets.demo['font'])
+        new Alphabet(this.assets.images.demo['font'](), [16, 32]),
+        new Spritesheet(this.assets.datasets.demo['font']())
     ]));
 }
 
